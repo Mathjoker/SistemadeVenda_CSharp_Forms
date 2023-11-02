cc9222529a51f31f859c9f23bcaaee22213d7855
Matheus Bitencourt <matheus_si_kta@outlook.com>
2023-11-02 12:45:24 -0300

    Inicio do Sistema

---
+++ frm_menu.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Sistema_de_Vendas
+{
+    partial class frm_menu
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.cadastrosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.produtosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.categoriaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btn_cad_produtos = new System.Windows.Forms.Button();
+            this.btn_cad_categoria = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cadastrosToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(953, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // cadastrosToolStripMenuItem
+            // 
+            this.cadastrosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.produtosToolStripMenuItem,
+            this.categoriaToolStripMenuItem});
+            this.cadastrosToolStripMenuItem.Name = "cadastrosToolStripMenuItem";
+            this.cadastrosToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.cadastrosToolStripMenuItem.Text = "Cadastros";
+            // 
+            // produtosToolStripMenuItem
+            // 
+            this.produtosToolStripMenuItem.Name = "produtosToolStripMenuItem";
+            this.produtosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.produtosToolStripMenuItem.Text = "Produtos";
+            this.produtosToolStripMenuItem.Click += new System.EventHandler(this.produtosToolStripMenuItem_Click);
+            // 
+            // categoriaToolStripMenuItem
+            // 
+            this.categoriaToolStripMenuItem.Name = "categoriaToolStripMenuItem";
+            this.categoriaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.categoriaToolStripMenuItem.Text = "Categorias";
+            this.categoriaToolStripMenuItem.Click += new System.EventHandler(this.categoriaToolStripMenuItem_Click);
+            // 
+            // btn_cad_produtos
+            // 
+            this.btn_cad_produtos.BackColor = System.Drawing.Color.Transparent;
+            this.btn_cad_produtos.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_cad_produtos.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_cad_produtos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_cad_produtos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_cad_produtos.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_cad_produtos.Image = global::Sistema_de_Vendas.Properties.Resources.cadastrar;
+            this.btn_cad_produtos.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btn_cad_produtos.Location = new System.Drawing.Point(39, 43);
+            this.btn_cad_produtos.Name = "btn_cad_produtos";
+            this.btn_cad_produtos.Size = new System.Drawing.Size(145, 154);
+            this.btn_cad_produtos.TabIndex = 2;
+            this.btn_cad_produtos.Text = "Cadastrar Produtos";
+            this.btn_cad_produtos.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_cad_produtos.UseVisualStyleBackColor = false;
+            this.btn_cad_produtos.Click += new System.EventHandler(this.btn_cad_produtos_Click);
+            // 
+            // btn_cad_categoria
+            // 
+            this.btn_cad_categoria.BackColor = System.Drawing.Color.Transparent;
+            this.btn_cad_categoria.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_cad_categoria.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_cad_categoria.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_cad_categoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_cad_categoria.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_cad_categoria.Image = global::Sistema_de_Vendas.Properties.Resources.Categoria;
+            this.btn_cad_categoria.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btn_cad_categoria.Location = new System.Drawing.Point(219, 43);
+            this.btn_cad_categoria.Name = "btn_cad_categoria";
+            this.btn_cad_categoria.Size = new System.Drawing.Size(153, 154);
+            this.btn_cad_categoria.TabIndex = 3;
+            this.btn_cad_categoria.Text = "Cadastrar Categorias ";
+            this.btn_cad_categoria.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_cad_categoria.UseVisualStyleBackColor = false;
+            this.btn_cad_categoria.Click += new System.EventHandler(this.btn_cad_categoria_Click);
+            // 
+            // frm_menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BackgroundImage = global::Sistema_de_Vendas.Properties.Resources.fundo2;
+            this.ClientSize = new System.Drawing.Size(953, 438);
+            this.Controls.Add(this.btn_cad_categoria);
+            this.Controls.Add(this.btn_cad_produtos);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frm_menu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Menu Principal";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem cadastrosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem produtosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem categoriaToolStripMenuItem;
+        private System.Windows.Forms.Button btn_cad_produtos;
+        private System.Windows.Forms.Button btn_cad_categoria;
+    }
+}
+
---
+++ frm_menu.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_de_Vendas
+{
+    public partial class frm_menu : Form
+    {
+        public frm_menu()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_cad_produtos_Click(object sender, EventArgs e)
+        {
+            frm_produtos frm = new frm_produtos();
+            frm.Show();
+        }
+
+        private void btn_cad_categoria_Click(object sender, EventArgs e)
+        {
+            frm_categorias frm = new frm_categorias();
+            frm.Show();
+        }
+
+        private void produtosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frm_produtos frm = new frm_produtos();
+            frm.Show();
+        }
+
+        private void categoriaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frm_categorias frm = new frm_categorias();
+            frm.Show();
+        }
+    }
+}
